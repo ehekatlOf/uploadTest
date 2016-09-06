d44fa86b5bf051ad3afe723d85f5138472a98cb6
ehekatlof <pranagy@gmail.com>
2016-09-06 01:22:37 +0000

    basic script

---
+++ scripts/upload.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $('#uploadInput').on('change', function() {
+        $('#fileDetail').text('ha caught one');
+    });
+
+    $('#submitFile').on('click', function() {
+
+    });
+});
+
+/*global $*/
